2ef81d862d2ac3c008e3c5223281c23ffe682647
Cory Parsnipson <cparsnipson@gmail.com>
2016-04-19 21:07:17 -0700

    Add TransformScene to repo.

---
+++ SFML_Test/Scene/TransformSceneGraphNode.cpp
@@ -0,0 +1,6 @@
+#include "TransformSceneGraphNode.h"
+
+TransformSceneGraphNode::TransformSceneGraphNode(sf::RenderStates state /* = sf::RenderStates::Default */, bool visible /* = true */)
+: SceneGraphNode(state, visible)
+{
+}
