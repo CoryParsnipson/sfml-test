ec2f28b1af63bce8954f7d6c50d5cf3b044f3f2b
Cory Parsnipson <cparsnipson@gmail.com>
2018-08-04 02:00:26 -0700

    Add rough running animation

--- levels/MegamanScene/MegamanScene.cpp
+++ levels/MegamanScene/MegamanScene.cpp
@@ -25,18 +25,37 @@ void MegamanScene::init(Game& game) {
    game.add_player(1);
 
    AnimationPtr stand_r = std::make_shared<Animation>("megaman_zero_stand_r", this->textures().get("megaman_zero_spritesheet"));
-   stand_r->add(sf::IntRect(0, 0, 34, 44), 120);
-   stand_r->add(sf::IntRect(34, 0, 34, 44), 3);
-   stand_r->add(sf::IntRect(68, 0, 34, 44), 120);
-   stand_r->add(sf::IntRect(102, 0, 34, 44), 3);
-   stand_r->add(sf::IntRect(136, 0, 34, 44), 3);
-   stand_r->add(sf::IntRect(170, 0, 34, 44), 3);
+   stand_r->add(sf::IntRect(0, 0, 34, 41), 120);
+   stand_r->add(sf::IntRect(34, 0, 34, 41), 3);
+   stand_r->add(sf::IntRect(68, 0, 34, 41), 120);
+   stand_r->add(sf::IntRect(102, 0, 34, 41), 3);
+   stand_r->add(sf::IntRect(136, 0, 34, 41), 3);
+   stand_r->add(sf::IntRect(170, 0, 34, 41), 3);
+
+   AnimationPtr run_r = std::make_shared<Animation>("megaman_zero_stand_r", this->textures().get("megaman_zero_spritesheet"));
+   //run_r->add(sf::IntRect(0, 41, 39, 39), 7); // this first frame is transition between standing and running
+   run_r->add(sf::IntRect(39, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(78, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(117, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(156, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(195, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(234, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(273, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(312, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(351, 41, 39, 39), 7);
+   run_r->add(sf::IntRect(390, 41, 39, 39), 7);
 
    Entity* character = this->create_entity("CharacterEntity");
    character->add<Sprite>("CharacterSprite");
    character->get<Sprite>()->animation(stand_r);
    character->get<Sprite>()->scale(3, 3);
    character->get<Sprite>()->position(100, 100);
+
+   Entity* character_run = this->create_entity("RunEntity");
+   character_run->add<Sprite>("RunSprite");
+   character_run->get<Sprite>()->animation(run_r);
+   character_run->get<Sprite>()->scale(3, 3);
+   character_run->get<Sprite>()->position(200, 100);
 }
 
 void MegamanScene::enter(Game& game) {
